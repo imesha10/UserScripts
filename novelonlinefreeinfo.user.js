683a64fb508350315540b3414ac28618ce758801
Avishka <imavmako@hotmail.com>
2018-02-01 22:24:23 -0600

    changed the version num

--- novelonlinefreeinfo.user.js
+++ novelonlinefreeinfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NOvelonlinefrEeinFoSimpLifierfiXer
 // @namespace    tools
-// @version      0.9
+// @version      0.91
 // @description  try to take over the world!
 // @author       IMAK
 // @match        http://novelonlinefree.info/*
