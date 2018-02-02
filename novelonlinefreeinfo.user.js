c4b7703ae16e5724c32077602bb954d8a89c756b
Avishka <imavmako@hotmail.com>
2018-02-01 22:36:45 -0600

    Fixed stuff

--- novelonlinefreeinfo.user.js
+++ novelonlinefreeinfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NOvelonlinefrEeinFoSimpLifierfiXer
 // @namespace    tools
-// @version      0.91
+// @version      0.95
 // @description  try to take over the world!
 // @author       IMAK
 // @match        http://novelonlinefree.info/*
@@ -10,29 +10,24 @@
 // ==/UserScript==
 
 var flag_00 = true; // Makes the lines have only one space from each other.
-var flag_01 = false; // shows the node types on screen
-var flag_02 = false; // shows the node value
+var flag_01 = false; // debug: shows the node types on screen
+var flag_02 = false; // debug: shows the node value
 $(document).ready(function(){
-    'use strict';
-    // Your code here...
-    var col = "rgb(0,0,0)";
-    document.getElementsByClassName("top_header")[0].remove();
-    document.getElementsByClassName("lem_bem")[0].remove();
-    document.getElementsByClassName("lem_bem_top")[0].remove();
-    document.getElementsByClassName("wrap-menu")[0].remove();
-    document.getElementsByClassName("custom_content")[0].remove();
-    document.getElementsByClassName("cmt_doc")[0].remove();
-    document.getElementsByClassName("menu_doc")[0].style.backgroundColor = col;
-    document.getElementsByClassName("name_chapter entry-title")[0].style.backgroundColor = col;
-    document.getElementsByClassName("menu_doc")[1].style.backgroundColor = col;
-    var x = document.getElementsByClassName("breadcrumbs");
-    x = [].slice.call(x);
-    [].forEach.call(x, function(a){
-        a.remove();
-    });
 
+    'use strict';
+    var col = "rgb(0,0,0)";
+    $(".top_header").remove();
+    $(".lem_bem").remove();
+    $(".lem_bem_top").remove();
+    $(".wrap-menu").remove();
+    $(".custom_content").remove();
+    $(".cmt_doc").remove();
+    $(".menu_doc").css('backgroundColor',col);
+    $(".name_chapter entry-title").css('backgroundColor',col);
+    $(".breadcrumbs").remove();
+
+    childNodes0 = $('#vung_doc')[0].childNodes;
     function check1(){
-        var childNodes0 = $('#vung_doc')[0].childNodes;
         var previousNodeType = 0;
         var counter = 0;
         for (var i = 0; i < childNodes0.length; i++){
@@ -47,13 +42,11 @@ $(document).ready(function(){
     }
 
     function check2(x){
-        var childNodes0 = $('#vung_doc')[0].childNodes;
         if (childNOdes.length<x+2 && childNodes0[x].nodeType == childNodes0[x+1].nodeType) return false;
         else return true;
     }
 
     if (flag_00 && check1()){
-        var childNodes0 = $('#vung_doc')[0].childNodes;
         for (var i = 0; i < childNodes0.length; i++){
             var node = childNodes0[i];
             if (node.nodeType != 3 && i%2==1 && check2(i)){node.remove(); i-=1;}
@@ -61,14 +54,12 @@ $(document).ready(function(){
     }
 
     if (flag_01){
-        var childNodes0 = $('#vung_doc')[0].childNodes;
         for (var i = 0; i < childNodes0.length; i++){
             console.log(childNodes0[i].nodeType);
         }
     }
 
     if (flag_02){
-        var childNodes0 = $('#vung_doc')[0].childNodes;
         for (var i = 0; i < childNodes0.length; i++){
             console.log(childNodes0[i].nodeValue);
         }
