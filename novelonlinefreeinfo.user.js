a179d3cc0fe2b0044e85aecfa763f685a0320b25
Avishka <imavmako@hotmail.com>
2018-02-17 21:11:41 -0600

    updated the vnum

--- novelonlinefreeinfo.user.js
+++ novelonlinefreeinfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NOvelonlinefrEeinFoSimpLifierfiXer
 // @namespace    tools
-// @version      0.993
+// @version      0.995
 // @description  try to take over the world!
 // @author       IMAK
 // @match        http://novelonlinefree.info/chapter/* 
