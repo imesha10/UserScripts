8ed7c275c1deb8dc261b59bab446e08ed52e22ef
Avishka <imavmako@hotmail.com>
2019-02-02 14:43:50 -0600

    wuxiamobilefix

---
+++ wuxiamobilefix.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         WuxiaWorld Mobile Read
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://m.wuxiaworld.co/*
+// @grant        none
+// ==/UserScript==
+
+$(document).ready(function(){
+    $('.Readpage')[0].remove();
+    $('.Readpage')[0].remove();
+});
