68fb014c23acf400b0da93036f173689b7c5c9e5
Avishka <imavmako@hotmail.com>
2019-05-06 19:50:50 -0500

    Reverted the change of time when excecuted

--- MangaTenseiFix.user.js
+++ MangaTenseiFix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MangaTenseiFix
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://github.com/imesha10/TamperMonkeyScripts/blob/master/MangaTenseiFix.user.js
@@ -9,9 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-
-
-window.addEventListener('load', function() {
+$(document).ready(function(){
     $(".container.chapter-nav.top")[0].childNodes[3].remove();
     $(".container.chapter-nav.top")[0].childNodes[2].remove();
     $(".container.chapter-nav.top")[0].childNodes[0].remove();
@@ -20,4 +18,5 @@ window.addEventListener('load', function() {
     $(".navbar.navbar-expand.navbar-dark")[0].remove();
     $(".sharethis-inline-follow-buttons.st-inline-follow-buttons")[0].remove();
     $(".container.chapter-page")[0].childNodes[3].remove();
-}, false);
+});
+
